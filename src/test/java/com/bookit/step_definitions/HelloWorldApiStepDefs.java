f2e88018e722b927aced975d4ef5d2eea62786fa
zivadinjo <zivadinjo1985@gmail.com>
2022-08-28 15:50:26 -0500

    Cucumber/API day 1

--- src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
@@ -5,38 +5,38 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers.*;
-import org.hamcrest.Matcher.*;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
-import java.net.URI;
-
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
 public class HelloWorldApiStepDefs {
+    public static final Logger LOG = LogManager.getLogger();
     String url = ConfigurationReader.getProperty("hello.world.api");
     Response response;
 
     @Given("User sends get request to hello world api")
     public void user_sends_get_request_to_hello_world_api() {
-       response = given().accept(ContentType.JSON)
+        LOG.info("Sending GET request to Hello World API" + url);
+
+        response = given().accept(ContentType.JSON)
                 .when().get(url);
+
+        LOG.info("Sending request completed with response" + response.asString());
     }
 
     @Then("hello world api status code is {int}")
     public void hello_world_api_status_code_is(int expectedStatusCode) {
-        System.out.println("response.statusCode() = " + response.statusCode());
-        System.out.println("expectedStatusCode = " + expectedStatusCode);
+        LOG.info("response.statusCode() = " + response.statusCode());
+        LOG.info("expectedStatusCode = " + expectedStatusCode);
         assertEquals(expectedStatusCode,response.statusCode());
     }
 
     @Then("hello world api response body contains {string}")
     public void hello_world_api_response_body_contains(String expMessage) {
-        response.prettyPeek();
+        response.prettyPrint();
         String actMessage = response.path("message");
         assertEquals("Verification failed",expMessage,actMessage);
     }
